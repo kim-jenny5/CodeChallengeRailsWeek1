0d6d37a51bac2f836046332a47642109008b661d
alexandru <boncut.alex10@gmail.com>
2021-07-09 11:01:31 -0500

    alex

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -16,17 +16,31 @@ class PartiesController < ApplicationController
         @party = Party.new(party_params)
         @party.save
         if @party.valid?
-        redirect_to parties_path
+            redirect_to parties_path
         else
             render :new
         end
 
     end
 
+    def edit
+        @party = Party.find_by_id(params[:id])
+    end
+
+    def update
+        @party = Party.find_by_id(params[:id])
+        @party.update(party_params)
+        if @party.valid?
+            redirect_to party_path(@party)
+        else
+            render :edit
+        end
+    end
+
     private
 
     def party_params
-    params.require(:party).permit(:name, :date, :category, :supplies, :budget, :private)
+        params.require(:party).permit(:name, :date, :category, :supplies, :budget)
     end
 
 end
--- app/models/party.rb
+++ app/models/party.rb
@@ -1,5 +1,16 @@
 class Party < ApplicationRecord
+    validates :name, :category, :supplies, presence: true
+    validates :date, uniqueness: true, presence: true
+    validates :budget, presence: true, numericality: {minimum: 100, maximum: 10000}
+    validate :starts_with_the
+
     def self.parties 
         self.order("name DESC") 
+    end
+
+    def starts_with_the
+        if !(name =~ /The/) == 0
+            errors.add(:name, "needs to start with 'The'")
+        end
     end
 end
